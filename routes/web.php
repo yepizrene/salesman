d15e16d6afbd6a55b7f122e35b8af157bcf14607
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 14:51:04 -0700

    modify / route redirected to login

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,19 @@
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-  return Inertia::render('Welcome', [
-    'canLogin' => Route::has('login'),
-    'canRegister' => Route::has('register'),
-    'laravelVersion' => Application::VERSION,
-    'phpVersion' => PHP_VERSION,
-  ]);
+
+  return Redirect::route('login');
+  // return Inertia::render('Welcome', [
+  //   'canLogin' => Route::has('login'),
+  //   'canRegister' => Route::has('register'),
+  //   'laravelVersion' => Application::VERSION,
+  //   'phpVersion' => PHP_VERSION,
+  // ]);
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
