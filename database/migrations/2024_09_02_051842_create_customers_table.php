f27e73b78141a3c8acda67b495d63acc89814fea
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 12:23:27 -0700

    modified email of customer unique

--- database/migrations/2024_09_02_051842_create_customers_table.php
+++ database/migrations/2024_09_02_051842_create_customers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     Schema::create('customers', function (Blueprint $table) {
       $table->increments('id');
       $table->string('name', 255);
-      $table->string('email', 50);
+      $table->string('email', 50)->unique();
       $table->string('phone', 50)->nullable();
       $table->string('address', 150)->nullable();
       $table->string('city', 50)->nullable();
