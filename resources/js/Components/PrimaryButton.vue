19be263f74f76926ad7e7e0ec2c8a9018d6b6b3c
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 14:51:24 -0700

    modify components of login to add register button

--- resources/js/Components/PrimaryButton.vue
+++ resources/js/Components/PrimaryButton.vue
@@ -8,8 +8,7 @@ defineProps({
 </script>
 
 <template>
-  <button :type="type"
-    class="inline-flex items-center px-4 py-2 bg-blueberry-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blueberry-700 focus:bg-blueberry-700 active:bg-blueberry-900 focus:outline-none focus:ring-2 focus:ring-blueberry-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
+  <button :type="type" class="inline-flex items-center px-4 py-2 bg-blueberry-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blueberry-700 focus:bg-blueberry-700 active:bg-blueberry-900 focus:outline-none focus:ring-2 focus:ring-blueberry-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150">
     <slot />
   </button>
 </template>
