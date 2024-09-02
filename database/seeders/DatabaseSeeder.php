c00b01b2e7f9580fddc360d23e7b8681ddf4b661
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 03:40:45 -0700

    modify seeder to create example users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'John Doe',
+            'email' => 'johndoe@example.com',
+            'password' => 'password',
         ]);
+
+        User::factory(28)->create();
     }
 }
