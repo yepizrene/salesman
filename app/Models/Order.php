0d2747369088e64fc228925c8befea92f7d0d25c
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 21:03:02 -0700

    Create order table, model, request, controller and migrations

---
+++ app/Models/Order.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Order extends Model
+{
+  use HasFactory;
+  use SoftDeletes;
+
+  protected $fillable = [
+    'customer_id',
+    'user_id'
+  ];
+
+  public function items(){
+    return $this->hasMany(OrderItem::class);
+  }
+
+  public function customer(){
+    return $this->belongsTo(Customer::class);
+  }
+
+  public function getCustomerName(){
+    if($this->customer){
+      return $this->customer->name;
+    }
+
+    return __('customer.working');
+  }
+
+  public function total(){
+    return $this->items->map(function($i){
+      return $i -> price;
+    });
+  }
+
+  public function formattedTotal(){
+    return number_format($this->total(),2);
+  }
+}
