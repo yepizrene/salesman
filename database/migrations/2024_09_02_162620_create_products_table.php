7003e1ab27cb2370de205aca967f5e7d7244e611
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 20:59:52 -0700

    indent products migration

--- database/migrations/2024_09_02_162620_create_products_table.php
+++ database/migrations/2024_09_02_162620_create_products_table.php
@@ -6,28 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->text('description')->nullable();
-            $table->decimal('price', 8, 2);
-            $table->integer('quantity')->default('1');
-            $table->boolean('status')->default(true);
-            $table->timestamps();
-            $table->softDeletes();
-        });
-    }
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('products', function (Blueprint $table) {
+      $table->id();
+      $table->string('name');
+      $table->text('description')->nullable();
+      $table->decimal('price', 8, 2);
+      $table->integer('quantity')->default('1');
+      $table->boolean('status')->default(true);
+      $table->timestamps();
+      $table->softDeletes();
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('products');
+  }
 };
