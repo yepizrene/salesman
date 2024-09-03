c9283d5fcaa12dc878003a066b9e746ff78dc3d7
Rene Yepiz <yepizrene@gmail.com>
2024-09-02 18:15:03 -0700

    complete crud of product, edit, update, destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,61 +10,63 @@ use Inertia\Inertia;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        $products = Product::paginate(5);
-        return Inertia::render('Products/Index', compact('products'));
-    }
+  /**
+   * Display a listing of the resource.
+   */
+  public function index()
+  {
+    $products = Product::paginate(5);
+    return Inertia::render('Products/Index', compact('products'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return Inertia::render('Products/Create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    return Inertia::render('Products/Create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreProductRequest $request)
-    {
-        Product::create($request->all());
-        return Redirect::route('products.index');
-    }
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(StoreProductRequest $request)
+  {
+    Product::create($request->all());
+    return Redirect::route('products.index');
+  }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Product $product)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   */
+  public function show(Product $product)
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Product $product)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   */
+  public function edit(Product $product)
+  {
+    return Inertia::render('Products/Edit', compact('product'));
+  }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateProductRequest $request, Product $product)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   */
+  public function update(UpdateProductRequest $request, Product $product)
+  {
+    $product->update($request->validated());
+    return Redirect::route('products.index')->with('message','Product has been updated!');
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Product $product)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   */
+  public function destroy(Product $product)
+  {
+    $product->delete();
+    return Redirect::route('products.index');
+  }
 }
