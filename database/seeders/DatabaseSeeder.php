72bc0c4a5823d2e95ac6e84c9820dbd076530cb4
Rene Yepiz <yepizrene@gmail.com>
2024-09-08 22:04:01 -0700

    modify database seeder to add a new guest user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,25 +8,30 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   */
+  public function run(): void
+  {
+    // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        User::factory()->create([
-            'name' => 'Administrator',
-            'email' => 'admin@gmail.com',
-            'password' => 'admin123',
-        ]);
+    // User::factory()->create([
+    //     'name' => 'Test User',
+    //     'email' => 'test@example.com',
+    // ]);
+    User::factory()->create([
+      'name' => 'Administrator',
+      'email' => 'admin@gmail.com',
+      'password' => 'admin123',
+    ]);
+    User::factory()->create([
+      'name' => 'Invitado',
+      'email' => 'invitado@gmail.com',
+      'password' => 'password',
+    ]);
 
-        User::factory(28)->create();
+    User::factory(28)->create();
 
-        $this->call(CustomerSeeder::class);
-    }
+    $this->call(CustomerSeeder::class);
+  }
 }
